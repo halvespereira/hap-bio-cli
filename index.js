d25a66479958adc40b3629542bcf80557d951433
Henrique Pereira <henrique.pereira@terazo.com>
2021-09-05 17:02:30 -0600

    improved ux

--- index.js
+++ index.js
@@ -2,6 +2,12 @@
 
 const pkgJSON = require("./package.json");
 const welcome = require("cli-welcome");
+const chalk = require("chalk");
+const linkedInClr = chalk.hex("#0077b5").bold.inverse;
+const gitHubClr = chalk.hex("#6cc644").bold.inverse;
+const instagramClr = chalk.hex("#833ab4").bold.inverse;
+const websiteClr = chalk.hex("#ff4c4c").bold.inverse;
+const dim = chalk.dim;
 
 welcome({
   title: `${pkgJSON.name}`,
@@ -15,13 +21,14 @@ welcome({
 });
 
 console.log(`
-Henrique Pereira - software engineer
+${chalk.hex("#ff4c4c").bold.inverse(` Henrique Pereira `)} 
 
+${chalk.italic(
+  `Technology enthusiast, originally from Brazil and currently living in different parts of the world at a time. Known for being a JavaScript developer enjoys learning new things. Feel free to reach out, I'm always available for a chat.`
+)} 
 
-Technology enthusiast, originally from Brazil but currently a citizen of the world. Known for being a JavaScript developer enjoys learning new things. 
-
-LinkedIn: https://www.linkedin.com/in/henrique-pereira/
-Instagram: @halveapereira
-GitHub: halveapereira
-Website: https://henriquepereira.com
+${linkedInClr(` LinkedIn `)} ${dim(`https://www.linkedin.com/in/henrique-pereira/`)}
+${gitHubClr(" Github ")} ${dim(`github.com/halveapereira`)}
+${instagramClr(" Instagram ")} ${dim(`halveapereira`)}
+${websiteClr(" Website ")} ${dim(`henriquepereira.com`)}
 `);
