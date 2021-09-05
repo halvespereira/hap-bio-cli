076ca13c175d2b6697445880ec23379ce1f94022
Henrique Pereira <henrique.pereira@terazo.com>
2021-09-05 15:47:12 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+console.log(`
+Henrique Pereira - software engineer
+
+
+Technology enthusiast, originally from Brazil but currently a citizen of the world. Known for being a JavaScript developer enjoys learning new things. 
+
+LinkedIn: https://www.linkedin.com/in/henrique-pereira/
+Instagram: @halveapereira
+GitHub: halveapereira
+
+`);
